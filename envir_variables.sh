98336d28b34d4cc850f086aa9352de4cfce69b65
Darwiish <inaaboowe@gmail.com>
2026-02-16 04:21:14 +0400

    Update  environment scripts

--- envir_variables.sh
+++ envir_variables.sh
@@ -3,11 +3,23 @@
 my_home="$HOME"
 my_user="$USER"
 my_os="$OSTYPE"
+my_username="$LOGNAME"
+my_shell="$SHELL"
+my_cd="$PWD"
+my_exP="$PATH"
+my_defult="$LANG-"
 
 echo "Home Directory: $my_home"
 echo "Current user: $my_user"
 echo "OS Type: $my_os"
 
+echo "Username: $my_username"
+echo "Shell: $my_shell"
+echo "Current Directory: $my_cd"
+
+echo "Executable Paths: "$my_exP"
+echo "Default Language: "$my_defult"
+
 #echo "Home Directory: $Home"
 #echo "Current user: $USER"
-#echo "OS Type: $OSTYPE"
+#echo "OS Type: "$OSTYPE"
