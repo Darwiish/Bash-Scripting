4765c747f3ea0267f30b08333228fbecef660821
Darwiish <inaaboowe@gmail.com>
2026-02-18 02:27:13 +0400

    Add bash script demonstrating file creation and reading techniques
    This script demonstrates different techniques for creating, writing, and reading files in Bash. Creates file1.txt using echo, file2.txt and file3.txt using heredoc. Implements read_file() to read line by line and process_file() to prefix each line with 'Processing line:'.

--- reading_files.sh
+++ reading_files.sh
@@ -12,6 +12,16 @@ Multiple Lines
 At once Using cat
 EOF
 
+echo
+
+cat >file3.txt <<EOF
+Focus on reading files and learn different techniques
+to extract data and process information from them. 
+Reading files is an important task in scripting. 
+It allows us to access and extract valuable information
+from various types of files.
+EOF
+
 read_file() {
 
 	local file_path="$1"
@@ -21,6 +31,16 @@ read_file() {
 	done <"$file_path"
 }
 
+echo
+
+process_file() {
+	local file_path="$1"
+	cat "$file_path" | while IFS= read -r line; do
+		echo "Processing line: $line"
+	done
+}
 read_file "./file1.txt"
 echo
 read_file "./file2.txt"
+echo
+process_file "./file3.txt"
